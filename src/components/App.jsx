04f528a00a44752ad08a4b33420c999f2f492a07
HelenaUa <delena.1408@gmail.com>
2023-02-24 18:36:35 +0200

    add all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,49 +12,68 @@ export class App extends Component {
     bad: 0,
   };
 
-  addGood = () => {
-    this.setState(prevState => {
-      return {
-        good: prevState.good + 1,
-      };
-    });
-  };
+  // addGood = () => {
+  //   this.setState(prevState => {
+  //     return {
+  //       good: prevState.good + 1,
+  //     };
+  //   });
+  // };
   
-  addNeutral = () => { 
-    this.setState(prevState => {
-      return {
-        neutral: prevState.neutral + 1,
-      };
-    });
+  // addNeutral = () => { 
+  //   this.setState(prevState => {
+  //     return {
+  //       neutral: prevState.neutral + 1,
+  //     };
+  //   });
+  // };
+
+  // addBad = () => {
+  //   this.setState(prevState => {
+  //     return {
+  //       bad: prevState.bad + 1,
+  //     };
+  //   });
+  //  };
+  
+  btnClick = (options) => {
+    this.setState({
+       [options.currentTarget.innerText]: this.state[options.currentTarget.innerText] + 1 
+    })
+  }
+  
+  countTotalFeedback = () => {
+    const total = this.state.good + this.state.neutral + this.state.bad;
+    return total;
   };
 
-  addBad = () => {
-    this.setState(prevState => {
-      return {
-        bad: prevState.bad + 1,
-      };
-    });
-   };
+  countPositiveFeedbackPercentage = () => {
+    let positivePercentage = 0;
+    if (!this.state.good || this.countTotalFeedback() === 0) {
+      return;
+    }
+     positivePercentage = this.state.good / this.countTotalFeedback() * 100;
+    return positivePercentage;
+  };
   
   render() {
     return (
       <div>
         <Section title="Plese leave feedback" />
         <FeedbackOptions
-          addGood={this.addGood}
-          addNeutral={this.addNeutral}
-          addBad={this.addBad}
-          // options={}
-          // onLeaveFeedback={ }
+          // addGood={this.addGood}
+          // addNeutral={this.addNeutral}
+          // addBad={this.addBad}
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.btnClick}
         />
         <Statistics 
           good={this.state.good}
           neutral={this.state.neutral}
           bad={this.state.bad}
-          // total={ }
-          // positivePercentage={ }
+          total={this.countTotalFeedback()}
+          positivePercentage={this.countPositiveFeedbackPercentage()}
           />
-          
     </div>
   );
   }
